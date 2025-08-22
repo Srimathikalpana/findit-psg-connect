ee049cd85325b5437facb414f7f64fc26b508654
Srimathikalpana <tsrimathikalpana@gmail.com>
2025-08-22 08:59:57 +0530

    login done

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();  // To load variables from .env
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB connected');
+})
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
